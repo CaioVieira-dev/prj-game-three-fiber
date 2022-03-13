d554c6ad07118dbccc0543c822772eb2a269bfe3
Caio Vieira <caiocesarav0208@gmail.com>
2022-03-13 13:41:56 -0300

    refactor: :recycle: refatora para usar hooks

--- src/hooks/usePlayerControls.ts
+++ src/hooks/usePlayerControls.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export const usePlayerControls = () => {
   const [left, setLeft] = useState(false);
@@ -6,26 +6,28 @@ export const usePlayerControls = () => {
   const [forward, setForward] = useState(false);
   const [backward, setBackward] = useState(false);
 
-  useEffect(() => {
-    function handleKeyDown(this: Document, e: KeyboardEvent) {
-      e.code === 'KeyA' && setLeft(true);
-      e.code === 'KeyD' && setRight(true);
-      e.code === 'KeyW' && setForward(true);
-      e.code === 'KeyS' && setBackward(true);
-    }
-    function handleKeyUp(this: Document, e: KeyboardEvent) {
-      e.code === 'KeyA' && setLeft(false);
-      e.code === 'KeyD' && setRight(false);
-      e.code === 'KeyW' && setForward(false);
-      e.code === 'KeyS' && setBackward(false);
-    }
+  const handleKeyDown = useCallback((e: KeyboardEvent) => {
+    e.code === 'KeyA' && setLeft(true);
+    e.code === 'KeyD' && setRight(true);
+    e.code === 'KeyW' && setForward(true);
+    e.code === 'KeyS' && setBackward(true);
+  }, []);
 
+  const handleKeyUp = useCallback((e: KeyboardEvent) => {
+    e.code === 'KeyA' && setLeft(false);
+    e.code === 'KeyD' && setRight(false);
+    e.code === 'KeyW' && setForward(false);
+    e.code === 'KeyS' && setBackward(false);
+  }, []);
+
+  useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
     document.addEventListener('keyup', handleKeyUp);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
       document.removeEventListener('keyup', handleKeyUp);
     };
-  }, []);
+  }, [handleKeyDown, handleKeyUp]);
+
   return { left, right, forward, backward };
 };
